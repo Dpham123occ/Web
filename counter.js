8f056627c8c19f05d0b1f7acf91da9734c937d82
DUY PHAM <d.pham1801@tcu.edu>
2023-04-13 22:46:34 -0500

    modified:   counter.js 	deleted:    main.html

--- counter.js
+++ counter.js
@@ -1,24 +1,25 @@
-// Get counter element and increment button element
-var counter = document.getElementById('counter');
-var incrementBtn = document.getElementById('incrementBtn');
+module.exports = (req, res) => {
+    // Check if the request method is POST
+    if (req.method === 'POST') {
+        // Get the current count from local storage
+        let count = parseInt(localStorage.getItem('count'));
 
-// Check if counter value exists in localStorage, if not, set to 0
-if (!localStorage.getItem('counterValue')) {
-    localStorage.setItem('counterValue', 0);
-}
+        // If count is null (i.e., no previous count found), set it to 0
+        if (isNaN(count)) {
+            count = 0;
+        }
 
-// Set initial counter value from localStorage
-counter.textContent = localStorage.getItem('counterValue');
+        // Increase the count by 1
+        count++;
 
-// Add click event listener to increment button
-incrementBtn.addEventListener('click', function() {
-    // Get current counter value from localStorage
-    var currentValue = parseInt(localStorage.getItem('counterValue'));
+        // Store the updated count in local storage
+        localStorage.setItem('count', count.toString());
 
-    // Increment counter value
-    currentValue++;
-
-    // Update counter element and localStorage with new value
-    counter.textContent = currentValue;
-    localStorage.setItem('counterValue', currentValue);
-});
+        // Send the updated count as the response
+        res.status(200).json({ count });
+    } else {
+        // Send the current count as the response for other HTTP methods
+        let count = parseInt(localStorage.getItem('count')) || 0;
+        res.status(200).json({ count });
+    }
+};
